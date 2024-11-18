4f8e1afb0c021293bed6a74c15610cb4feb6020a
GuruPruthviJM <gurupruthvi61@gmail.com>
2024-11-18 13:46:23 +0530

    Used own custom React Element

--- 01_Basics of React/src/main.jsx
+++ 01_Basics of React/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  'Click me to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <p>This is done by using the custom react element: {reactElement}</p>
   </>
 )
