c996b9181325f4afac010fb474acbdf8b8339791
GuruPruthviJM <gurupruthvi61@gmail.com>
2024-11-18 13:57:30 +0530

    customReactElement

--- 01_Basics of React/src/main.jsx
+++ 01_Basics of React/src/main.jsx
@@ -1,19 +1,21 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+const anotherElement = "| Guru"
 const reactElement = React.createElement(
   'a',
   {
     href: 'https://www.google.com',
     target: '_blank'
   },
-  'Click me to visit google'
+  'Click me to visit google ',
+  anotherElement // We can use children in both the methods
 )
 
 createRoot(document.getElementById('root')).render(
   <>
     <App />
-    <p>This is done by using the custom react element: {reactElement}</p>
+    <p>This is done by using the custom react element: {reactElement}</p> 
+    <p>We use evaluated expression ('Flower Brackets') for mentioning the variable</p>
   </>
 )
