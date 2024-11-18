bee8c59014cd4c1e2fe46f5dfbebd853b9e0b1e2
GuruPruthviJM <gurupruthvi61@gmail.com>
2024-11-18 06:14:14 +0530

    Create and Delete Vite-react Project

---
+++ 01_Basics of React/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <>
+    <h1>Chai aur React with vite | Guru Pruthvi J M</h1>
+    <p>
+      Process to create vite React
+      <ul>
+        <li>npm create vite@latest</li>
+        <li>Enter the project name and package name</li>
+        <li>Select the framework as React and the varient as Java Script</li>
+        <li>cd vite-project</li>
+        <li>npm install</li>
+        <li>npm run dev</li>
+      </ul>
+    </p>
+    <p>Process to delete react
+      <ul>
+        <li>Remove-Item -Recurse -Force "folder name"</li>
+      </ul>
+    </p>
+    </>
+  )
+}
+
+export default App
